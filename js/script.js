576006cfc958f3b88e71f8a43b6a9e8ee439c9f1
Andrew <avenuce@rambler.ru>
2017-05-16 20:21:33 +0300

    slider donne

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = function (){
+		var imgs = document.querySelectorAll('.slide'), len = imgs.length, i = len-1;
+  (function go(){
+     imgs[i].classList.remove('active')
+     i = ++i % len;
+     imgs[i].classList.add('active');
+     window.setTimeout(go, 1000)
+  })()
+}
